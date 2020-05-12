0cf7fb7aafe6c0ae09c3a7bf15d68f486fbe7050
Aaron Southammavong <aaroneight07@gmail.com>
2020-05-12 13:39:46 -0500

    setting up class component App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      title: 'Terror Apps',
+      headerLinks: [
+        {
+          title: 'Home',
+          path: '/',
+        },
+        {
+          title: 'About',
+          path: '/about',
+        },
+        {
+          title: 'Contact',
+          path: '/contact',
+        },
+      ],
+      home: {
+        title: 'Be Relentless',
+        subTitile: 'Projects that make a difference',
+        description: 'See projects below',
+      },
+      about: {
+        title: 'About Us',
+      },
+      contact: {
+        title: 'Contact Us',
+      },
+    };
+  }
+
   render() {
     return (
       <div className="App">
