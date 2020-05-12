0415a9f1b617bb924f9fa67af5a59346d9da5048
Aaron Southammavong <aaroneight07@gmail.com>
2020-05-12 17:17:13 -0500

    setup HomePage to pass in props correctly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,21 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const Hero = () => {
+const Hero = (props) => {
   return (
-    <Jumbotron>
-      <Container>
-        <Row>
-          <Col>
-            <h2>Terror Apps</h2>
+    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+      <Container fluid={true}>
+        <Row className="justify-content-center py-5">
+          <Col md={8} sm={12}>
+            {props.title && (
+              <h1 className="display-1 font-weight-bold">{props.title}</h1>
+            )}
+            {props.subTitle && (
+              <h3 className="display-4 font-weight-light">{props.subTitle}</h3>
+            )}
+            {props.description && (
+              <h3 className="lead font-weight-light">{props.description} </h3>
+            )}
           </Col>
         </Row>
       </Container>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import Hero from '../components/Hero.jsx';
 
 const HomePage = (props) => {
-  return <Hero />;
+  return (
+    <Hero
+      title={props.title}
+      subTitle={props.subTitle}
+      description={props.description}
+    />
+  );
 };
 
 export default HomePage;
